abde308e6757cdd7d5d2def3dd5b962ca7e3bed0
odekhtiarenko <oleg.dekhtiarenko@gmail.com>
2023-11-20 13:32:06 +0100

    Project structure

---
+++ FundaTestAssessment.Api/Controllers/RealEstateAgentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FundaTestAssessment.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class RealEstateAgentController : Controller
+    {
+        [HttpGet("{location}/top-most-active")]
+        public async Task<IActionResult> GetTopActive(string location, CancellationToken cancellationToken, string? filter = null)
+        {
+            return Ok();
+        }
+    }
+}
+
